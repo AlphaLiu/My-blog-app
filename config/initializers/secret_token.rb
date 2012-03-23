862fbc5035fc4eb945f4ab9d82d27f4b9ae46bda
Alpha Liu <alpha.liu@me.com>
2012-03-23 17:07:20 +0800

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBlogApp::Application.config.secret_token = '6bb00e8213e4884617a8520834da41a74d3027c57d2399647fd904e6e2ff95776caddee47879532bf7ce8b88cdf07ac99f8e95f4b355cbb6ff91beb3ef46d5d6'
